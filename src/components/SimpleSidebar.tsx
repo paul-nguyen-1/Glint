7a122c26737c52243605d5f5219a92dee8b5617d
paul-nguyen96 <paul121596@icloud.com>
2023-05-20 23:30:53 -0500

    set up light/dark mode

--- src/components/SimpleSidebar.tsx
+++ src/components/SimpleSidebar.tsx
@@ -13,6 +13,7 @@ import {
     useDisclosure,
     BoxProps,
     FlexProps,
+    Button,
 } from '@chakra-ui/react';
 import {
     FiHome,
@@ -25,18 +26,20 @@ import {
 } from 'react-icons/fi';
 import { IconType } from 'react-icons';
 import logo from '/glint-logo.png'
+import ColorModeToggle from './ColorModeToggle';
 
 interface LinkItemProps {
     name: string;
     icon: IconType;
+    path: string;
 }
 const LinkItems: Array<LinkItemProps> = [
-    { name: 'Home', icon: FiHome },
-    { name: 'Trending', icon: FiTrendingUp },
-    { name: 'Explore', icon: FiCompass },
-    { name: 'Favourites', icon: FiStar },
-    { name: 'Settings', icon: FiSettings },
-    { name: 'Profile', icon: FiUser }
+    { name: 'Home', icon: FiHome, path: '/' },
+    { name: 'Trending', icon: FiTrendingUp, path: 'Trending' },
+    { name: 'Explore', icon: FiCompass, path: 'Explore' },
+    { name: 'Favorites', icon: FiStar, path: 'Favorites' },
+    { name: 'Settings', icon: FiSettings, path: 'Setting' },
+    { name: 'Profile', icon: FiUser, path: "Profile" }
 ];
 
 export default function SimpleSidebar({ children }: { children: ReactNode }) {
@@ -84,15 +87,21 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
             {...rest}>
             <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
                 <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
-                    <img src={logo} alt={logo} />
+                    <img src={logo} alt={logo} style={{ height: "100px" }} />
                 </Text>
+                    <ColorModeToggle/>
                 <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
             </Flex>
             {LinkItems.map((link) => (
-                <NavItem key={link.name} icon={link.icon}>
+                <NavItem key={link.name} icon={link.icon} path={link.path}>
                     {link.name}
                 </NavItem>
             ))}
+            <Flex justifyContent="center" mt="200px">
+                <Button colorScheme='messenger' variant='solid' width="100px">
+                    Login
+                </Button>
+            </Flex>
         </Box>
     );
 };
@@ -100,35 +109,39 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
 interface NavItemProps extends FlexProps {
     icon: IconType;
     children: string;
+    path: string;
 }
-const NavItem = ({ icon, children, ...rest }: NavItemProps) => {
+const NavItem = ({ icon, children, path, ...rest }: NavItemProps) => {
     return (
-        <Link href={children} style={{ textDecoration: 'none' }} _focus={{ boxShadow: 'none' }}>
-            <Flex
-                align="center"
-                p="4"
-                mx="4"
-                borderRadius="lg"
-                role="group"
-                cursor="pointer"
-                _hover={{
-                    bg: 'cyan.400',
-                    color: 'white',
-                }}
-                {...rest}>
-                {icon && (
-                    <Icon
-                        mr="4"
-                        fontSize="16"
-                        _groupHover={{
-                            color: 'white',
-                        }}
-                        as={icon}
-                    />
-                )}
-                {children}
-            </Flex>
-        </Link>
+        <div>
+
+            <Link href={path} style={{ textDecoration: 'none' }} _focus={{ boxShadow: 'none' }}>
+                <Flex
+                    align="center"
+                    p="4"
+                    mx="4"
+                    borderRadius="lg"
+                    role="group"
+                    cursor="pointer"
+                    _hover={{
+                        bg: 'cyan.400',
+                        color: 'white',
+                    }}
+                    {...rest}>
+                    {icon && (
+                        <Icon
+                            mr="4"
+                            fontSize="16"
+                            _groupHover={{
+                                color: 'white',
+                            }}
+                            as={icon}
+                        />
+                    )}
+                    {children}
+                </Flex>
+            </Link>
+        </div>
     );
 };
 
@@ -157,6 +170,7 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
             <Text fontSize="2xl" ml="8" fontFamily="monospace" fontWeight="bold">
                 <img src={logo} alt={logo} style={{ height: "100px" }} />
             </Text>
+            <ColorModeToggle/>
         </Flex>
     );
 };
